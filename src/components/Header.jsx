7bc317347a51788f4c352ed7e41910577a793bb4
CerfSoleil <robedj12@gmail.com>
2025-04-22 10:26:32 -0500

    header.jsx | Name included as title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import "../styles/Header.css";
 const Header = () => {
   return (
     <header className="site-header">
-      <h1 className="header-title">Your Name Here</h1>
+      <h1 className="header-title">Robert Duncan Jones III</h1>
     </header>
   );
 };
