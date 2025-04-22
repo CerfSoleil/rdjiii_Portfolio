b9193ae569b5d42779b1dcc3991481d5c19daa39
CerfSoleil <robedj12@gmail.com>
2025-04-22 10:01:56 -0500

    Footer.jsx | Links updated, Instagram removed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,16 @@ import "../styles/Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/CerfSoleil" target="_blank" rel="noopener noreferrer">
       <img className="footer-icon" src="/images/icons/githubLogo.png" alt="GitHub" />
       </a>
-      <a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">
+      <a href="https://linkedin.com/in/rdj-iii" target="_blank" rel="noopener noreferrer">
         <img className="footer-icon" src="/images/icons/linkedinLogo.png" alt="LinkedIn" />
       </a>
-      <a href="https://instagram.com/" target="_blank" rel="noopener noreferrer">
+      {/* Instagram doesn't come included unless I'm pursuing/working purely creative opportunities. */}
+      {/* <a href="https://instagram.com/" target="_blank" rel="noopener noreferrer">
       <img className="footer-icon" src="/images/icons/instagramLogo.png" alt="Instagram" />
-      </a>
+      </a> */}
     </footer>
   );
 };
