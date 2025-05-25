829f18f91a558134bd1cfd5f0e289c21d69cf4fc
CerfSoleil <robedj12@gmail.com>
2025-05-25 10:28:13 -0500

    AngelsAirwaves.jsx | Removed logic

--- src/pages/AngelsAirwaves.jsx
+++ src/pages/AngelsAirwaves.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "../styles/PortfolioGallery.css";
 
+import Gallery from "../components/Gallery";
+
 const images = [
   { title: "Throne", src: "/images/photography_galleries/angels-airwaves/IMG_3521 Throne.png", description: "Description of image 2" },
   { title: "Seraphim", src: "/images/photography_galleries/angels-airwaves/IMG_3535 Seraphim.png", description: "Description of image 2" },
@@ -11,43 +13,7 @@ const images = [
 ];
 
 const AngelsAirwaves = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  const openImagePreview = (image) => {
-    setSelectedImage(image);
-  };
-
-  const closeImagePreview = () => {
-    setSelectedImage(null);
-  };
-
-  return (
-    <div className="portfolio-gallery">
-      <h1>Angels & Airwaves Collection</h1>
-      <div className="image-gallery">
-        {images.map((image, index) => (
-          <div key={index} className="image-tile" onClick={() => openImagePreview(image)}>
-            <img src={image.src} alt={image.title} className="gallery-image" />
-            <div className="image-title">{image.title}</div>
-          </div>
-        ))}
-      </div>
-
-      {selectedImage && (
-        <div className="image-preview">
-          <div className="preview-overlay" onClick={closeImagePreview}></div>
-          <div className="preview-content">
-            <img src={selectedImage.src} alt={selectedImage.title} className="expanded-image" />
-            <div className="image-info">
-              <h3>{selectedImage.title}</h3>
-              <p>{selectedImage.description}</p>
-              <button className="close-btn" onClick={closeImagePreview}>X</button>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+  return <Gallery title="Angels & Airwaves Collection" images={images} />;
 };
 
 export default AngelsAirwaves;
