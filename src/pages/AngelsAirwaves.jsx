51b1aac17ca60f91fc6d4d9366559da6b3432777
CerfSoleil <robedj12@gmail.com>
2025-05-25 11:40:22 -0500

    Descriptions added to images

--- src/pages/AngelsAirwaves.jsx
+++ src/pages/AngelsAirwaves.jsx
@@ -4,12 +4,12 @@ import "../styles/PortfolioGallery.css";
 import Gallery from "../components/Gallery";
 
 const images = [
-  { title: "Throne", src: "/images/photography_galleries/angels-airwaves/IMG_3521 Throne.png", description: "Description of image 2" },
-  { title: "Seraphim", src: "/images/photography_galleries/angels-airwaves/IMG_3535 Seraphim.png", description: "Description of image 2" },
-  { title: "Behind the Gates", src: "/images/photography_galleries/angels-airwaves/IMG_3579 Behind the Gates.png", description: "Description of image 2" },
-  { title: "Powers", src: "/images/photography_galleries/angels-airwaves/IMG_3598 Powers.png", description: "Description of image 2" },
-  { title: "Cherubim", src: "/images/photography_galleries/angels-airwaves/IMG_3714 Cherubim.png", description: "Description of image 2" },
-  { title: "Virtue", src: "/images/photography_galleries/angels-airwaves/IMG_3839 Virtue.png", description: "Description of image 2" },
+  { title: "Throne", src: "/images/photography_galleries/angels-airwaves/IMG_3521 Throne.png", description: "Roxborough, PA Tower Farm | Taken from the freeway in the early morning rush.\nDate: 12/13/24" },
+  { title: "Seraphim", src: "/images/photography_galleries/angels-airwaves/IMG_3535 Seraphim.png", description: "Wyndmoor, PA | Wyndmoor Tower\nDate: 12/13/24" },
+  { title: "Behind the Gates", src: "/images/photography_galleries/angels-airwaves/IMG_3579 Behind the Gates.png", description: "Closter, New Jersey | Near a pretty cliff overlook of New York\nDate: 12/13/24" },
+  { title: "Powers", src: "/images/photography_galleries/angels-airwaves/IMG_3598 Powers.png", description: "Merrimack, NH | WFEA Tower on a clear, pretty day.\nDate: 12/14/24" },
+  { title: "Cherubim", src: "/images/photography_galleries/angels-airwaves/IMG_3714 Cherubim.png", description: "Batavia, NY\nDate: 12/15/24" },
+  { title: "Virtue", src: "/images/photography_galleries/angels-airwaves/IMG_3839 Virtue.png", description: "Mesquite, TX | Mesquite Tower, the prettiest angel I've seen yet.\nDate: 12/31/24" },
 ];
 
 const AngelsAirwaves = () => {
--- src/pages/SunsetCement.jsx
+++ src/pages/SunsetCement.jsx
@@ -4,20 +4,20 @@ import "../styles/PortfolioGallery.css";
 import Gallery from "../components/Gallery";
 
 const images = [
-  { title: "Arrivals", src: "/images/photography_galleries/sunsets-cement/Arrivals_3.jpg", description: "Description of image 1" },
-  { title: "Rolling In", src: "/images/photography_galleries/sunsets-cement/IMG_2563 Rolling In.jpg", description: "Description of image 2" },
-  { title: "Sun's Departure", src: "/images/photography_galleries/sunsets-cement/IMG_2569 Sun's Departure.jpg", description: "Description of image 3" },
-  { title: "The Sunway", src: "/images/photography_galleries/sunsets-cement/IMG_2836 The Sunway.jpg", description: "Description of image 4" },
-  { title: "Red Eye", src: "/images/photography_galleries/sunsets-cement/IMG_2905 Red Eye.jpg", description: "Description of image 5" },
-  { title: "Fried Chicken", src: "/images/photography_galleries/sunsets-cement/IMG_2943 Fried Chicken.jpg", description: "Description of image 5" },
-  { title: "Logistic Rise", src: "/images/photography_galleries/sunsets-cement/IMG_3045 Logistic Rise.jpg", description: "Description of image 5" },
-  { title: "Backseat Burn", src: "/images/photography_galleries/sunsets-cement/IMG_3052 Backseat Burn.jpg", description: "Description of image 5" },
-  { title: "Trash Day", src: "/images/photography_galleries/sunsets-cement/IMG_3085 Trash Day.jpg", description: "Description of image 5" },
-  { title: "What is Home", src: "/images/photography_galleries/sunsets-cement/IMG_3132 What is Home.jpg", description: "Description of image 5" },
-  { title: "Sun Window", src: "/images/photography_galleries/sunsets-cement/IMG_3419 Sun Window.jpg", description: "Description of image 5" },
-  { title: "Road Blind", src: "/images/photography_galleries/sunsets-cement/IMG_3471 Road Blind.jpg", description: "Description of image 5" },
-  { title: "Under the Bridge", src: "/images/photography_galleries/sunsets-cement/IMG_3660 Under the Bridge.jpg", description: "Description of image 5" },
-  { title: "It's Over Isn't It", src: "/images/photography_galleries/sunsets-cement/IMG_3841 It's Over Isn't It on Brick.jpg", description: "Description of image 5" },
+  { title: "Arrivals", src: "/images/photography_galleries/sunsets-cement/Arrivals_3.jpg", description: "Houston, TX Northbound on-ramp to Beltway 8 from IAH airport\nDate: 2018" },
+  { title: "Rolling In", src: "/images/photography_galleries/sunsets-cement/IMG_2563 Rolling In.jpg", description: "Houston, TX\nDate: 3/5/24" },
+  { title: "Sun's Departure", src: "/images/photography_galleries/sunsets-cement/IMG_2569 Sun's Departure.jpg", description: "Houston, TX over IAH airport\nDate: 4/30/24" },
+  { title: "The Sunway", src: "/images/photography_galleries/sunsets-cement/IMG_2836 The Sunway.jpg", description: "Houston, TX over IAH airport\nDate: 4/30/24" },
+  { title: "Red Eye", src: "/images/photography_galleries/sunsets-cement/IMG_2905 Red Eye.jpg", description: "Houston, TX over IAH airport/nDate: 6/30/24" },
+  { title: "Fried Chicken", src: "/images/photography_galleries/sunsets-cement/IMG_2943 Fried Chicken.jpg", description: "Houston, TX\nDate: 7/11/24" },
+  { title: "Logistic Rise", src: "/images/photography_galleries/sunsets-cement/IMG_3045 Logistic Rise.jpg", description: "Somewhere in North Carolina\nDate: 7/14/24" },
+  { title: "Backseat Burn", src: "/images/photography_galleries/sunsets-cement/IMG_3052 Backseat Burn.jpg", description: "Somewhere in North Carolina\nDate: 7/14/24" },
+  { title: "Trash Day", src: "/images/photography_galleries/sunsets-cement/IMG_3085 Trash Day.jpg", description: "Norfolk, VA\nDate: 7/16/24" },
+  { title: "What is Home", src: "/images/photography_galleries/sunsets-cement/IMG_3132 What is Home.jpg", description: "Houston, TX\nDate: 7/26/24" },
+  { title: "Sun Window", src: "/images/photography_galleries/sunsets-cement/IMG_3419 Sun Window.jpg", description: "On the road through Missouri or Illinois\nDate: 12/4/24" },
+  { title: "Road Blind", src: "/images/photography_galleries/sunsets-cement/IMG_3471 Road Blind.jpg", description: "Somewhere in Northern Ohio\nDate: 12/10/24" },
+  { title: "Under the Bridge", src: "/images/photography_galleries/sunsets-cement/IMG_3660 Under the Bridge.jpg", description: "Somewhere in Northern New York State\nDate:12/14/24" },
+  { title: "It's Over Isn't It", src: "/images/photography_galleries/sunsets-cement/IMG_3841 It's Over Isn't It on Brick.jpg", description: "Dallas, TX behind a school\nDate: 12/31/24" },
 ];
 
 const SunsetCement = () => {
