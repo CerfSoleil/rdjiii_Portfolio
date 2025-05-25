13eccb5e06890b45301e981237f1ca94830eaa11
CerfSoleil <robedj12@gmail.com>
2025-05-25 10:10:31 -0500

    C:/Program Files/Git/png folder removed, SunsetCement.jsx images added

--- src/pages/SunsetCement.jsx
+++ src/pages/SunsetCement.jsx
@@ -2,11 +2,20 @@ import React, { useState } from "react";
 import "../styles/PortfolioGallery.css";
 
 const images = [
-  { title: "Image 1", src: "/images/IMG_3538.jpg", description: "Description of image 1" },
-  { title: "Image 2", src: "/images/sunset-cement2.jpg", description: "Description of image 2" },
-  { title: "Image 3", src: "/images/sunset-cement3.jpg", description: "Description of image 3" },
-  { title: "Image 4", src: "/images/sunset-cement4.jpg", description: "Description of image 4" },
-  { title: "Image 5", src: "/images/sunset-cement5.jpg", description: "Description of image 5" },
+  { title: "Arrivals", src: "/images/photography_galleries/sunsets-cement/Arrivals_3.jpg", description: "Description of image 1" },
+  { title: "Rolling In", src: "/images/photography_galleries/sunsets-cement/IMG_2563 Rolling In.jpg", description: "Description of image 2" },
+  { title: "Sun's Departure", src: "/images/photography_galleries/sunsets-cement/IMG_2569 Sun's Departure.jpg", description: "Description of image 3" },
+  { title: "The Sunway", src: "/images/photography_galleries/sunsets-cement/IMG_2836 The Sunway.jpg", description: "Description of image 4" },
+  { title: "Red Eye", src: "/images/photography_galleries/sunsets-cement/IMG_2905 Red Eye.jpg", description: "Description of image 5" },
+  { title: "Fried Chicken", src: "/images/photography_galleries/sunsets-cement/IMG_2943 Fried Chicken.jpg", description: "Description of image 5" },
+  { title: "Logistic Rise", src: "/images/photography_galleries/sunsets-cement/IMG_3045 Logistic Rise.jpg", description: "Description of image 5" },
+  { title: "Backseat Burn", src: "/images/photography_galleries/sunsets-cement/IMG_3052 Backseat Burn.jpg", description: "Description of image 5" },
+  { title: "Trash Day", src: "/images/photography_galleries/sunsets-cement/IMG_3085 Trash Day.jpg", description: "Description of image 5" },
+  { title: "What is Home", src: "/images/photography_galleries/sunsets-cement/IMG_3132 What is Home.jpg", description: "Description of image 5" },
+  { title: "Sun Window", src: "/images/photography_galleries/sunsets-cement/IMG_3419 Sun Window.jpg", description: "Description of image 5" },
+  { title: "Road Blind", src: "/images/photography_galleries/sunsets-cement/IMG_3471 Road Blind.jpg", description: "Description of image 5" },
+  { title: "Under the Bridge", src: "/images/photography_galleries/sunsets-cement/IMG_3660 Under the Bridge.jpg", description: "Description of image 5" },
+  { title: "It's Over Isn't It", src: "/images/photography_galleries/sunsets-cement/IMG_3841 It's Over Isn't It on Brick.jpg", description: "Description of image 5" },
 ];
 
 const SunsetCement = () => {
