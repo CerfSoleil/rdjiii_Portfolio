c5da0af2eb8cf84f5e3185cca9fc78a7b066b9cd
CerfSoleil <robedj12@gmail.com>
2025-05-30 11:11:43 -0500

    CodingPortfolio.jsx | Refactor for further utilization

---
+++ src/components/CodingRepositories.jsx
@@ -0,0 +1,23 @@
+export const repositories = [
+    { 
+        title: "AutoScout Car Dealership", 
+        description: "A mockup website emulating a used car dealership's page. Includes functionality for customers and employees", 
+        repo: "https://github.com/andrewPelfrey/used-car-dealer" ,
+        deploy: "",
+        images: ["",""]
+    },
+    { 
+        title: "WordéMon", 
+        description: "An interactive PokéMon fan project.", 
+        repo: "https://github.com/nolangrossi/Wordemon",
+        deploy: "",
+        images: ["",""] 
+    },
+    { 
+        title: "Project 3", 
+        description: "Description of Project 3", 
+        repo: "https://github.com",
+        deploy: "",
+        images: ["",""] 
+    },
+  ];
--- src/pages/CodingPortfolio.jsx
+++ src/pages/CodingPortfolio.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+
+import { repositories } from "../components/CodingRepositories";
+import CodingGallery from "../components/CodingGallery";
+
 import "../styles/CodingPortfolio.css";
 
 // Change the layout to have links for both the deploy and the live demo as well as a scrolling preview of a few pages.
@@ -6,26 +10,26 @@ import "../styles/CodingPortfolio.css";
 // Include WordéMon (wordemon.onrender.com) & (github.com/nolangrossi/Wordemon). To be changed wehn I resume work on it.
   // Also try to get Wordemon simultaneously running on the website.
 
-const repositories = [
-  { title: "AutoScout Car Dealership", description: "A mockup website emulating a used car dealership's page. Includes functionality for customers and employees", link: "https://github.com/andrewPelfrey/used-car-dealer" },
-  { title: "WordéMon", description: "An interactive PokéMon fan project.", link: "https://github.com/nolangrossi/Wordemon" },
-  { title: "Project 3", description: "Description of Project 3", link: "https://github.com" },
-];
+
 
 const CodingPortfolio = () => {
   return (
     <div className="coding-portfolio">
       {repositories.map((repo, index) => (
-        <a
-          key={index}
-          href={repo.link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="coding-tile"
-        >
+        <div key={index} className="coding-tile">
+          <CodingGallery images={repo.images} />
           <h3>{repo.title}</h3>
           <p>{repo.description}</p>
-        </a>
+          <div className="coding-links">
+            <a href={repo.repo} target="_blank" rel="noopener noreferrer">Repo</a>
+            {" | "}
+            {repo.deploy ? (
+              <a href={repo.deploy} target="_blank" rel="noopener noreferrer">Deploy</a>
+            ) : (
+              <span>[App not Currently Deployed]</span>
+            )}
+          </div>
+        </div>
       ))}
     </div>
   );
