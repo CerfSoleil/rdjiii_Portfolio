8a5d981475ad794fc4db7c8401c527ea83ac3e91
CerfSoleil <robedj12@gmail.com>
2025-05-30 12:49:44 -0500

    images/coding_galleries/ | Added images gallery for coding

--- src/components/CodingRepositories.jsx
+++ src/components/CodingRepositories.jsx
@@ -2,22 +2,28 @@ export const repositories = [
     { 
         title: "AutoScout Car Dealership", 
         description: "A mockup website emulating a used car dealership's page. Includes functionality for customers and employees", 
+        activeDev: false,
         repo: "https://github.com/andrewPelfrey/used-car-dealer" ,
         deploy: "",
-        images: ["",""]
+        video: "",
+        images: ["../images/coding_galleries/Wordemon/Wordemon_1.png","../images/coding_galleries/Wordemon/Wordemon_2.png","../images/coding_galleries/Wordemon/Wordemon_3.png"]
     },
     { 
         title: "WordéMon", 
-        description: "An interactive PokéMon fan project.", 
+        description: "An interactive PokéMon fan project.",
+        activeDev: false,
         repo: "https://github.com/nolangrossi/Wordemon",
         deploy: "",
+        video: "",
         images: ["",""] 
     },
     { 
         title: "Project 3", 
         description: "Description of Project 3", 
+        activeDev: false,
         repo: "https://github.com",
         deploy: "",
+        video: "",
         images: ["",""] 
     },
   ];
