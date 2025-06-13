3c9ddc8c9e9b095b1d3e199e3a46be9828ff4d93
CerfSoleil <robedj12@gmail.com>
2025-06-12 20:10:56 -0500

    AboutMe.jsx | Filled in AboutMe section

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -6,7 +6,19 @@ const AboutMe = () => {
     <div className="about-me">
       <h1>About Me</h1>
       <p>
-        This is a sample paragraph for the "About Me" page. This will be filled in with relevant information later.
+        I am a multidisciplinary creative and full-stack web developer who thrives at the intersection of design, code, and storytelling. With a background in visual arts, writing, and the human element, I approach each project balancing technical execution and artistic insight to create an engaging product.
+      </p>
+      <br></br>
+      <p>
+        As a developer, I specialize in building user-centered applications from front to back. Using modern tools and frameworks to bring ideas to life in responsive, scalable, and elegant ways. Whether I'm designing interfaces or architecting full systems, I do so with both creativity and clarity.
+      </p>
+      <br></br>
+      <p>
+        Beyond the technical, I am a people-first collaborator who values communication, empathy, and individual growth. In leadership roles, I cultivate fulfilling environments where every contributor feels heard and positively engaged. I believe the best work and healthiest workforce emerges when people feel that their accomplishments are celebrated, their small changes are complemented, and their grievances both listened to and handled.
+      </p>
+      <br></br>
+      <p>
+        Curious, adaptable, and driven by connection, I see every project as an opportunity to fuse disciplines, elevate ideas, and build something meaningful - together.
       </p>
     </div>
   );
