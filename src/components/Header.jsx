72232a241f41637bbc846091e9d4284b94efde49
CerfSoleil <robedj12@gmail.com>
2025-06-19 18:47:02 -0500

    Added header image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,9 @@ import "../styles/Header.css";
 const Header = () => {
   return (
     <header className="site-header">
+      <div className="header-element">
+        <img className="header-image" src="../images/photography_galleries/IMG_0781s.jpg" alt="Header" />
+      </div>
       <h1 className="header-title">Robert Duncan Jones III</h1>
     </header>
   );
