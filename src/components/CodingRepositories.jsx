7b9c738387b096bbda85a196470e9103c2f1604d
CerfSoleil <robedj12@gmail.com>
2025-06-25 18:18:41 -0500

    Added readmegen to the coding portfolio

--- src/components/CodingRepositories.jsx
+++ src/components/CodingRepositories.jsx
@@ -29,7 +29,7 @@ export const repositories = [
         activeDev: false,
         repo: "https://github.com/CerfSoleil/Readme-Generator",
         deploy: "",
-        video: "https://private-user-images.githubusercontent.com/181133166/391756755-6476b0df-2140-449a-86c7-ccad1b9a6f4b.mp4?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NTA4ODc4NDAsIm5iZiI6MTc1MDg4NzU0MCwicGF0aCI6Ii8xODExMzMxNjYvMzkxNzU2NzU1LTY0NzZiMGRmLTIxNDAtNDQ5YS04NmM3LWNjYWQxYjlhNmY0Yi5tcDQ_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNjI1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDYyNVQyMTM5MDBaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0xZjNiNTE4MzhhYTMxYWQ5ZDIyMTNlMDk3YWU2MDdlNDBhOWY1NGFkZmNiMDNlYWU1MzA2MjdmYTNjOGFlZmJhJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.H2IEvgSUGHChATyQDI7qo7zcxHYgos4ffIb-Olb8M5Y",
-        images: [""]
+        video: "../videos/readmegen_showcase.mp4",
+        images: ["../images/coding_galleries/readmegen/ReadmeGen.png"]
     }
   ];
